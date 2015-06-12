f92e0402bc7607e2f6aa6b765f67a98774395d02
jnphilipp <me@jnphilipp.org>
2015-06-12 18:38:31 +0200

    Added language model to admin.

--- association/admin.py
+++ association/admin.py
@@ -1,3 +1,19 @@
+from accounts.models import Language, TextFieldSingleLine
 from django.contrib import admin
+from django.db.models import Count
+from django.forms import TextInput
 
-# Register your models here.
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'updated_at')
+    readonly_fields = ('slug',)
+    search_fields = ('name', 'code')
+
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off'})},
+    }
+
+    fieldsets = [
+        (None, {'fields': ['slug', 'name', 'code']}),
+    ]
+
+admin.site.register(Language, LanguageAdmin)
