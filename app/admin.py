a0e7cc65da71fb51b5add791dde693f8137559ec
jnphilipp <me@jnphilipp.org>
2015-06-14 23:01:15 +0200

    Updated app admin.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,31 @@
+from app.models import AssociationHistory, Profile, TextFieldSingleLine
 from django.contrib import admin
+from django.forms import TextInput
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cultural_background', 'points')
+    search_fields = ('user__username', 'cultural_background')
+
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off'})},
+    }
+
+    fieldsets = [
+        (None, {'fields': ['user', 'cultural_background', 'points']}),
+    ]
+
+class AssociationHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'association', 'points')
+    list_filter = ('user', 'association__word__languages', 'association__word', 'association__association')
+    search_fields = ('user__username', 'association__word__name', 'association__association__name')
+
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off'})},
+    }
+
+    fieldsets = [
+        (None, {'fields': ['user', 'association', 'points']}),
+    ]
+
+admin.site.register(AssociationHistory, AssociationHistoryAdmin)
+admin.site.register(Profile, ProfileAdmin)
