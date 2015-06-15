0e8bb7aff62038a67fcd42194ab37feb3657ed15
jnphilipp <me@jnphilipp.org>
2015-06-15 23:29:44 +0200

    Added leaderboard.

--- TIMA/urls.py
+++ TIMA/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'association.views.home', name='home'),
-    url(r'^(?P<slug>[\w-]+)/association$', 'association.views.association', name='association'),
+    url(r'^languages/(?P<slug>[\w-]+)/association$', 'association.views.association', name='association'),
+
+    url(r'^leaderboard/$', 'app.views.leaderboard.leaderboard', name='leaderboard'),
 
     url(r'^profile/$', 'app.views.profile.profile', name='profile'),
     url(r'^profile/association_history/$', 'app.views.profile.association_history', name='association_history'),
---
+++ app/views/leaderboard.py
@@ -0,0 +1,17 @@
+from app.models import Profile
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
+def leaderboard(request):
+    profile_list = Profile.objects.all().order_by('-points')
+
+    paginator = Paginator(profile_list, 50)
+    page = request.GET.get('page')
+    try:
+        profiles = paginator.page(page)
+    except PageNotAnInteger:
+        profiles = paginator.page(1)
+    except EmptyPage:
+        profiles = paginator.page(paginator.num_pages)
+
+    return render(request, 'tima/leaderboard/leaderboard.html', locals())
