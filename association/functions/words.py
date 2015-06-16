df8661fa36ffaa597c10a0c3b53813673490774a
jnphilipp <me@jnphilipp.org>
2015-06-16 09:21:31 +0200

    Fixed selectin function.

--- association/functions/words.py
+++ association/functions/words.py
@@ -3,6 +3,6 @@ from django.db.models import Count
 
 def get_next_word(language, user=None):
     if user:
-        return Word.objects.filter(languages=language).filter(word__associationhistory__user=user).annotate(associationhistory_count=Count('word__associationhistory')).order_by('associationhistory_count', 'count', '?').first()
+        return Word.objects.filter(languages=language).extra(select={'associationhistory_count': 'SELECT COUNT(*) FROM app_associationhistory JOIN association_association ON association_association.id=app_associationhistory.association_id WHERE association_association.word_id=association_word.id AND user_id=%s'}, select_params=[user.id]).order_by('associationhistory_count', 'count', '?').first()
     else:
         return Word.objects.filter(languages=language).order_by('count', '?').first()
