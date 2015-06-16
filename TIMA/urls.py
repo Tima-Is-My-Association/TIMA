582661817b777a578f5ff78b1019961a30249fcf
jnphilipp <me@jnphilipp.org>
2015-06-16 14:30:38 +0200

    Added password change urls/templates.

--- TIMA/urls.py
+++ TIMA/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     url(r'^profile/$', 'app.views.profile.profile', name='profile'),
     url(r'^profile/association_history/$', 'app.views.profile.association_history', name='association_history'),
     url(r'^profile/edit/$', 'app.views.profile.edit', name='profile_edit'),
+    url(r'^profile/edit/password/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    url(r'^profile/edit/password/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
 
     url(r'^signin/$', 'app.views.base.signin', name='signin'),
     url(r'^signout/$', 'django.contrib.auth.views.logout', name='signout'),
---
+++ app/templatetags/tima/__init__.py
@@ -0,0 +1,4 @@
+from django.template import Library
+register = Library()
+
+from .base import *
