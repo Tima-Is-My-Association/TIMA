1835c8646749204a48d0e1e57f6559094a260d6c
jnphilipp <me@jnphilipp.org>
2015-06-18 09:17:18 +0200

    Updated graph.

--- association/views/api.py
+++ association/views/api.py
@@ -8,10 +8,20 @@ def graph(request, word_id):
 
     nodes = []
     links = []
+    n = {}
+    n[word.id] = len(nodes)
     nodes.append({'id':word.id, 'name':word.name, 'group':0})
-    for association in word.word.all():
-        nodes.append({'id':association.association.id, 'name':association.association.name, 'group':association.count})
-        links.append({'source':0, 'target':len(nodes) - 1, 'value':association.count})
+    for a in word.word.all():
+        if not a.association.id in n:
+            n[a.association.id] = len(nodes)
+            nodes.append({'id':a.association.id, 'name':a.association.name, 'group':a.count})
+        links.append({'source':n[word.id], 'target':n[a.association.id], 'value':a.count})
+        for b in a.association.word.all():
+            if not b.association.id in n:
+                n[b.association.id] = len(nodes)
+                nodes.append({'id':b.association.id, 'name':b.association.name, 'group':b.count})
+            links.append({'source':n[a.association.id], 'target':n[b.association.id], 'value':b.count})
+
     data = {'nodes':nodes, 'links':links}
 
     mimetype = 'application/json'
