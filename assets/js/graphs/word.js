fbcfd475ed8c7746635c83dd226b7bbb6f9dd717
jnphilipp <me@jnphilipp.org>
2015-06-18 15:22:36 +0200

    Replaced lines with arrows.

--- assets/js/graphs/word.js
+++ assets/js/graphs/word.js
@@ -1,6 +1,12 @@
-var r = 8;
 var width = $("#graph").width(), height = window.innerHeight - 220;
 var fill = d3.scale.category20();
+
+var r = 10,
+    markerWidth = 10,
+    markerHeight = 10,
+    refX = 2*r,
+    refY = -0.5,
+    drSub = r + 5;
 
 d3.json(url, function(data) {
     var linkedByIndex = {};
@@ -20,11 +26,26 @@ d3.json(url, function(data) {
             .attr("width", width)
             .attr("height", height);
 
-    var link = svg.selectAll("g.link")
+    svg.append("svg:defs").selectAll("marker")
+            .data(["end"])
+            .enter().append("svg:marker")
+            .attr("id", String)
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", refX)
+            .attr("refY", refY)
+            .attr("markerWidth", markerWidth)
+            .attr("markerHeight", markerHeight)
+            .attr("markerUnits", "userSpaceOnUse")
+            .attr("orient", "auto")
+            .append("svg:path")
+            .attr("d", "M0,-5L10,0L0,5");
+
+    var path = svg.selectAll("g.path")
             .data(data.links)
-            .enter().append("svg:line")
+            .enter().append("svg:path")
             .attr("class", "link")
-            .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+            .style("stroke-width", function(d) { return Math.sqrt(d.value); })
+            .attr("marker-end", "url(#end)");
 
     var node_drag = d3.behavior.drag()
             .on("dragstart", dragstart)
@@ -54,10 +75,13 @@ d3.json(url, function(data) {
             .text(function(d) { return d.name; });
 
     function tick() {
-        link.attr("x1", function(d) { return d.source.x; })
-                .attr("y1", function(d) { return d.source.y; })
-                .attr("x2", function(d) { return d.target.x; })
-                .attr("y2", function(d) { return d.target.y; });
+        path.attr("d", function (d) {
+            var dx = d.target.x - d.source.x,
+                dy = (d.target.y - d.source.y),
+                dr = Math.sqrt(dx * dx + dy * dy);
+            return "M" + d.source.x + "," + d.source.y + "A" + (dr - drSub) + "," + (dr - drSub) + " 0 0,1 " + d.target.x + "," + d.target.y;
+        });
+
         node.attr("transform", function(d) { return "translate(" + Math.max(r, Math.min(width - r, d.x)) + "," + Math.max(r, Math.min(height - r, d.y)) + ")"; });
     }
 
@@ -74,7 +98,7 @@ d3.json(url, function(data) {
         node.transition().duration(750).style("opacity", function (o) {
             return neighboring(d, o) | neighboring(o, d) ? 1 : 0.1;
         });
-        link.transition().duration(750).style("opacity", function (o) {
+        path.transition().duration(750).style("opacity", function (o) {
             return d.index===o.source.index | d.index===o.target.index ? 1 : 0.1;
         });
     }
@@ -88,7 +112,7 @@ d3.json(url, function(data) {
                 .attr("x", 12)
                 .style("font-size", "10px");
         node.transition().duration(750).style("opacity", 1);
-        link.transition().duration(750).style("opacity", 1);
+        path.transition().duration(750).style("opacity", 1);
     }
 
     function neighboring(a, b) { return linkedByIndex[a.index + "," + b.index]; }
