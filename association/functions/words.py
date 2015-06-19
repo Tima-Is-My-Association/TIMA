42fd70c3da60f3fc96051a77274f1d95932be8a5
jnphilipp <me@jnphilipp.org>
2015-06-19 14:18:10 +0200

    Added icon.

--- association/functions/words.py
+++ association/functions/words.py
@@ -8,7 +8,7 @@ def get_next_word(language, user=None, excludes=None):
         words = words.exclude(id=exclude.id)
 
     if user and not user.is_anonymous():
-        words = words.extra(select={'associationhistory_count': 'SELECT COUNT(*) FROM app_associationhistory JOIN association_association ON association_association.id=app_associationhistory.association_id WHERE association_association.word_id=association_word.id AND user_id=%s'}, select_params=[user.id]).order_by('associationhistory_count', 'count')[:15]
+        words = words.extra(select={'associationhistory_count': 'SELECT COUNT(*) FROM app_associationhistory JOIN association_association ON association_association.id=app_associationhistory.association_id WHERE association_association.word_id=association_word.id AND user_id=%s'}, select_params=[user.id]).order_by('associationhistory_count')[:15]
     else:
         words = words.order_by('count')[:15]
     return words[randint(0,14 if words.count() == 15 else words.count())]
