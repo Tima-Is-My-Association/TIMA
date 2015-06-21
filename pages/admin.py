98171f8e6e16cbf4156ba39945a9a59cd77293ed
jnphilipp <me@jnphilipp.org>
2015-06-21 23:12:26 +0200

    Added admin for pages.

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.forms import TextInput
+from django.db import models
+from pages.models import Page, TextFieldSingleLine
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    ordering = ('title',)
+    readonly_fields = ('slug',)
+
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off'})},
+    }
+
+    fieldsets = [
+        (None, {'fields': ['slug', 'title']}),
+        ('Text', {'fields': ['text'], 'classes': ('full-width',)}),
+    ]
+
+admin.site.register(Page, PageAdmin)
