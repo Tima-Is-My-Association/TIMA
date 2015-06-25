84bc90cd22356a26849b9f642b77ecf43853adaa
jnphilipp <me@jnphilipp.org>
2015-06-25 15:36:29 +0200

    Added GEt request to next word.

--- association/views/api/words.py
+++ association/views/api/words.py
@@ -8,34 +8,47 @@ from json import dumps
 
 @csrf_exempt
 def next(request):
-    """Handels a POST request for the next word.
+    """Handels a POST/GET request for the next word.
 
     POST parameters:
     language --- language of the word
     username --- username of a user
     excludes --- list of words that should be exclude from the result (optinal)
     """
+
+    language = None
+    user = None
+    excludes = []
     if request.method == 'POST':
         language = get_object_or_404(Language,
             code=request.POST.get('language'))
 
-        user = None
         if 'username' in request.POST:
             user = get_object_or_404(get_user_model(),
                 username=request.POST.get('username'))
 
-        excludes = []
         if 'excludes' in request.POST:
             excludes = Word.objects.filter(name__in=
                 request.POST.getlist('excludes'))
+    elif request.method == 'GET':
+        language = get_object_or_404(Language,
+            code=request.GET.get('language'))
 
-        word = get_next_word(language)
-        data = {'word': word.name}
-        mimetype = 'application/json'
+        if 'username' in request.GET:
+            user = get_object_or_404(get_user_model(),
+                username=request.GET.get('username'))
 
-        return HttpResponse(dumps(data), mimetype)
+        if 'excludes' in request.GET:
+            excludes = Word.objects.filter(name__in=
+                request.GET.getlist('excludes'))
     else:
          return HttpResponseBadRequest()
+
+    word = get_next_word(language, user, excludes)
+    data = {'word': word.name}
+    mimetype = 'application/json'
+
+    return HttpResponse(dumps(data), mimetype)
 
 def graph(request, word_id):
     word = get_object_or_404(Word, id=word_id)
