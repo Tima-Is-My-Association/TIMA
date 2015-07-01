6a447aaedb3db46dcca134d893312033b6db3833
jnphilipp <me@jnphilipp.org>
2015-07-01 16:26:36 +0200

    Bug fix.

--- association/functions/words.py
+++ association/functions/words.py
@@ -18,7 +18,7 @@ def get_next_word(language, user=None, excludes=[]):
         words = words.extra(select={'associationhistory_count': 'SELECT COUNT(*) FROM app_associationhistory JOIN association_association ON association_association.id=app_associationhistory.association_id WHERE association_association.word_id=association_word.id AND user_id=%s'}, select_params=[user.id]).order_by('associationhistory_count')[:15]
     else:
         words = words.order_by('count')[:15]
-    return words[randint(0,14 if words.count() == 15 else words.count())]
+    return words[randint(0, (14 if words.count() == 15 else (words.count() - 1)))]
 
 def build_graph(word, depth=2):
     nodes = []
