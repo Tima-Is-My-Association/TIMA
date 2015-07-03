8126d228affe841491c8ea4397693664c33f1843
jnphilipp <me@jnphilipp.org>
2015-07-03 19:21:33 +0200

    Added request ListIdentifiers and updates.

--- oai_pmh/templatetags/oai_pmh/base.py
+++ oai_pmh/templatetags/oai_pmh/base.py
@@ -2,29 +2,39 @@ from oai_pmh.templatetags.oai_pmh import register
 from datetime import datetime, timedelta
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from oai_pmh.models import ResumptionToken
+from django.utils import timezone
+from oai_pmh.models import MetadataFormat, ResumptionToken, Set
 from os import urandom
+
+@register.simple_tag
+def list_request_params(request):
+    params = request.POST if request.method == 'POST' else request.GET
+    return ' '.join(['%s="%s"' % (param, params.get(param)) for param in params])
 
 @register.simple_tag
 def timestamp(format_string):
     if format_string == 'UTC':
         return datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-    return datetime.now().strftime(format_string)
+    return timezone.now().strftime(format_string)
 
 @register.simple_tag
 def baseurl():
     return '%s%s' % (settings.ALLOWED_HOSTS[0], reverse('oai2'))
 
 @register.simple_tag
-def adminemails():
+def admin_emails():
     return '\n'.join(['<adminEmail>%s</adminEmail>' % admin[1] for admin in settings.ADMINS])
 
 @register.simple_tag
-def resumptionToken(paginator, page):
+def resumption_token(paginator, page, metadata_prefix=None, set_spec=None, from_timestamp=None, until_timestamp=None):
     if paginator.num_pages > 0 and page.has_next():
-        expirationDate = datetime.utcnow() + timedelta(days=1)
+        expiration_date = datetime.utcnow() + timedelta(days=1)
         token = ''.join('%02x' % i for i in urandom(16))
-        ResumptionToken.objects.create(token=token, expiration_date=expirationDate, complete_list_size=paginator.count, cursor=page.end_index())
-        return '<resumptionToken expirationDate="%s" completeListSize="%s" cursor="%s">%s</resumptionToken>' % (expirationDate.strftime('%Y-%m-%dT%H:%M:%SZ'), paginator.count, page.end_index(), token)
+
+        metadata_format = MetadataFormat.objects.get(prefix=metadata_prefix) if metadata_prefix else None
+        set_spec = Set.objects.get(spec=set_spec) if set_spec else None
+
+        ResumptionToken.objects.create(token=token, expiration_date=expiration_date, complete_list_size=paginator.count, cursor=page.end_index(), metadata_prefix=metadata_format, set_spec=set_spec, from_timestamp=from_timestamp, until_timestamp=until_timestamp)
+        return '<resumptionToken expirationDate="%s" completeListSize="%s" cursor="%s">%s</resumptionToken>' % (expiration_date.strftime('%Y-%m-%dT%H:%M:%SZ'), paginator.count, page.end_index(), token)
     else:
         return ''
--- oai_pmh/views.py
+++ oai_pmh/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
@@ -16,65 +17,107 @@ def oai2(request):
     global PER_PAGE
 
     errors = []
-    params = request.POST if request.method == 'POST' else request.GET
-    p = params.copy()
+    from_timestamp = None
+    until_timestamp = None
+    params = request.POST.copy() if request.method == 'POST' else request.GET.copy()
 
     if 'verb' in params:
-        verb = p.pop('verb')[0]
+        verb = params.pop('verb')[-1]
         if verb == 'Identify':
-            checkBadArguments(p, errors)
             template = 'tima/oai_pmh/identify.xml'
-                return render(request, 'tima/oai_pmh/error.xml', locals(), content_type='text/xml')
+            check_bad_arguments(params, errors)
+        elif verb == 'ListIdentifiers':
+            template = 'tima/oai_pmh/listidentifiers.xml'
+
+            if 'resumptionToken' in params:
+                header_list = Header.objects.all()
+                paginator, headers = do_resumption_token(params, errors, header_list)
+            elif 'metadataPrefix' in params:
+                metadata_prefix = params.pop('metadataPrefix')[-1]
+                if not MetadataFormat.objects.filter(prefix=metadata_prefix).exists():
+                    errors.append({'code':'cannotDisseminateFormat', 'msg':'The value of the metadataPrefix argument "%s" is not supported.' % metadata_prefix})
+                else:
+                    header_list = Header.objects.filter(metadata_formats__prefix=metadata_prefix)
+
+                    if 'set' in params:
+                        if Set.objects.all().count() == 0:
+                            errors.append({'code':'noSetHierarchy', 'msg':'This repository does not support sets.'})
+                        else:
+                            set_spec = params.pop('set')[-1]
+                            header_list = header_list.filter(sets__spec=set_spec)
+                    if 'from' in params:
+                        from_timestamp = datetime.strptime(params.pop('from')[-1] + ' +0000', '%Y-%m-%dT%H:%M:%SZ %z')
+                        header_list = header_list.filter(timestamp__gte=from_timestamp)
+                    if 'until' in params:
+                        until_timestamp = datetime.strptime(params.pop('until')[-1] + ' +0000', '%Y-%m-%dT%H:%M:%SZ %z')
+                        header_list = header_list.filter(timestamp__lte=until_timestamp)
+
+                    if header_list.count() == 0:
+                        errors.append({'code':'noRecordsMatch', 'msg':'The combination of the values of the from, until, and set arguments results in an empty list.'})
+                    else:
+                        paginator = Paginator(header_list, PER_PAGE)
+                        headers = paginator.page(1)
+            else:
+                errors.append({'code':'badArgument', 'msg':'The required argument "metadataPrefix" is missing in the request.'})
+            check_bad_arguments(params, errors)
         elif verb == 'ListMetadataFormats':
+            template = 'tima/oai_pmh/listmetadataformats.xml'
             metadataformats = MetadataFormat.objects.all()
 
             if 'identifier' in params:
-                identifier = p.pop('identifier')[0]
+                identifier = params.pop('identifier')[-1]
                 if Header.objects.filter(identifier=identifier).exists():
                     metadataformats = metadataformats.filter(identifiers__identifier=identifier)
                 else:
                     errors.append({'code':'idDoesNotExist', 'msg':'A record with the identifier "%s" does not exist.' % identifier})
-
             if metadataformats.count() == 0:
                 errors.append({'code':'noMetadataFormats', 'msg':'There are no metadata formats available for the record with identifier "%s".' % identifier})
-            checkBadArguments(p, errors)
-            template = 'tima/oai_pmh/listmetadataformats.xml'
+            check_bad_arguments(params, errors)
         elif verb == 'ListSets':
-            set_list = Set.objects.all()
             template = 'tima/oai_pmh/listsets.xml'
 
-            if set_list.count() == 0:
+            if not Set.objects.all().exists():
                 errors.append({'code':'noSetHierarchy', 'msg':'This repository does not support sets.'})
             else:
-                paginator = Paginator(set_list, PER_PAGE)
-                sets = doResumptionToken(params, p, errors, paginator)
-            checkBadArguments(p, errors)
+                paginator, sets = do_resumption_token(params, errors, Set.objects.all())
+            check_bad_arguments(params, errors)
         else:
             errors.append({'code':'badVerb', 'msg':'The verb "%s" provided in the request is illegal.' % verb})
     else:
         errors.append({'code':'badVerb', 'msg':'The request does not provide any verb.'})
     return render(request, template if not errors else 'tima/oai_pmh/error.xml', locals(), content_type='text/xml')
 
-def checkBadArguments(p, errors, msg=None):
-    for k,v in p.copy().items():
+def check_bad_arguments(params, errors, msg=None):
+    for k,v in params.copy().items():
         errors.append({'code':'badArgument', 'msg':'The argument "%s" (value="%s") included in the request is not valid.%s' % (k, v, (' %s' % msg if msg else ''))})
-        p.pop(k)
+        params.pop(k)
 
-def doResumptionToken(params, p, errors, paginator):
+def do_resumption_token(params, errors, objs):
     if 'resumptionToken' in params:
-        resumptionToken = p.pop('resumptionToken')[0]
+        resumption_token = params.pop('resumptionToken')[-1]
         try:
-            rt = ResumptionToken.objects.get(token=resumptionToken)
+            rt = ResumptionToken.objects.get(token=resumption_token)
             if timezone.now() > rt.expiration_date:
-                errors.append({'code':'badResumptionToken', 'msg':'The resumptionToken "%s" is expired.' % resumptionToken})
+                errors.append({'code':'badResumptionToken', 'msg':'The resumptionToken "%s" is expired.' % resumption_token})
             else:
+                if rt.set_spec:
+                    objs = objs.filter(sets=rt.set_spec)
+                if rt.metadata_prefix:
+                    objs = objs.filter(metadata_formats=rt.metadata_prefix)
+                if rt.from_timestamp:
+                    objs = objs.filter(timestamp__gte=rt.from_timestamp)
+                if rt.until_timestamp:
+                    objs = objs.filter(timestamp__gte=rt.until_timestamp)
+
+                paginator = Paginator(objs, PER_PAGE)
                 try:
                     page = paginator.page(rt.cursor / PER_PAGE + 1)
                 except EmptyPage:
-                    errors.append({'code':'badResumptionToken', 'msg':'The resumptionToken "%s" is invalid.' % resumptionToken})
+                    errors.append({'code':'badResumptionToken', 'msg':'The resumptionToken "%s" is invalid.' % resumption_token})
         except ResumptionToken.DoesNotExist:
-            errors.append({'code':'badResumptionToken', 'msg':'The resumptionToken "%s" is invalid.' % resumptionToken})
-        checkBadArguments(p, errors, msg='The usage of resumptionToken as an argument allows no other arguments.')
+            errors.append({'code':'badResumptionToken', 'msg':'The resumptionToken "%s" is invalid.' % resumption_token})
+        check_bad_arguments(params, errors, msg='The usage of resumptionToken as an argument allows no other arguments.')
     else:
+        paginator = Paginator(objs, PER_PAGE)
         page = paginator.page(1)
-    return page
+    return (paginator, page)
