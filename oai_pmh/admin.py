846a866838a048ab9075633d58df83c5134d7a32
jnphilipp <me@jnphilipp.org>
2015-07-03 08:58:15 +0200

    Updated oai_pmh admin.

--- oai_pmh/admin.py
+++ oai_pmh/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.forms import TextInput
+from oai_pmh.models import Header, TextFieldSingleLine
 
-# Register your models here.
+class HeaderAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'timestamp', 'deleted')
+    list_filter = ('timestamp', 'deleted')
+    readonly_fields = ('timestamp',)
+    search_fields = ('identifier',)
+
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off'})},
+    }
+
+    fieldsets = [
+        (None, {'fields': ['identifier', 'timestamp', 'deleted']}),
+    ]
+
+admin.site.register(Header, HeaderAdmin)
