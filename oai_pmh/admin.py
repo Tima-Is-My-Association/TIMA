cf2a7b27383c65d1074195227c7a8e0a30b1c6df
jnphilipp <me@jnphilipp.org>
2015-07-03 14:17:29 +0200

    Updated oai_pmh.admin.

--- oai_pmh/admin.py
+++ oai_pmh/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.forms import TextInput
-from oai_pmh.models import Header, TextFieldSingleLine
+from oai_pmh.models import Header, ResumptionToken, TextFieldSingleLine
 
 class HeaderAdmin(admin.ModelAdmin):
     list_display = ('identifier', 'timestamp', 'deleted')
@@ -16,4 +16,18 @@ class HeaderAdmin(admin.ModelAdmin):
         (None, {'fields': ['identifier', 'timestamp', 'deleted']}),
     ]
 
+class ResumptionTokenAdmin(admin.ModelAdmin):
+    list_display = ('token', 'expiration_date', 'complete_list_size', 'cursor')
+    list_filter = ('expiration_date',)
+    search_fields = ('token', 'complete_list_size', 'cursor')
+
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off'})},
+    }
+
+    fieldsets = [
+        (None, {'fields': ['token', 'expiration_date', 'complete_list_size', 'cursor']}),
+    ]
+
 admin.site.register(Header, HeaderAdmin)
+admin.site.register(ResumptionToken, ResumptionTokenAdmin)
