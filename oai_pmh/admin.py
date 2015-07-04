26956151371b7a6128301f9531af81af4ace91d4
jnphilipp <me@jnphilipp.org>
2015-07-04 08:16:45 +0200

    Updated admin.

--- oai_pmh/admin.py
+++ oai_pmh/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from django.db import models
 from django.forms import TextInput, Textarea
-from oai_pmh.models import Header, MetadataFormat, ResumptionToken, Set, TextFieldSingleLine
+from oai_pmh.models import DCRecord, Header, MetadataFormat, ResumptionToken, Set, TextFieldSingleLine
+
+class DCRecordAdmin(admin.ModelAdmin):
+    list_display = ('dc_identifier', 'dc_title', 'dc_creator')
+    list_filter = ('dc_date',)
+    readonly_fields = ('dc_date',)
+    search_fields = ('dc_identifier', 'dc_title', 'dc_creator', 'dc_subject', 'dc_description', 'dc_publisher', 'dc_contributor', 'dc_date', 'dc_type', 'dc_format', 'dc_identifier', 'dc_source', 'dc_language', 'dc_relation', 'dc_coverage', 'dc_rights')
+
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off'})},
+    }
+
+    fieldsets = [
+        (None, {'fields': ['header', 'dc_title', 'dc_creator', 'dc_subject', 'dc_description', 'dc_publisher', 'dc_contributor', 'dc_date', 'dc_type', 'dc_format', 'dc_identifier', 'dc_source', 'dc_language', 'dc_relation', 'dc_coverage', 'dc_rights']}),
+    ]
+
+class DCRecordInline(admin.StackedInline):
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off'})},
+    }
+
+    model = DCRecord
 
 class HeaderAdmin(admin.ModelAdmin):
     list_display = ('identifier', 'timestamp', 'deleted')
@@ -20,6 +41,7 @@ class HeaderAdmin(admin.ModelAdmin):
     ]
 
     filter_horizontal = ('metadata_formats', 'sets')
+    inlines = [DCRecordInline,]
 
 class MetadataFormatAdmin(admin.ModelAdmin):
     list_display = ('prefix', 'schema', 'namespace')
@@ -60,6 +82,7 @@ class SetAdmin(admin.ModelAdmin):
         (None, {'fields': ['name', 'spec', 'description']}),
     ]
 
+admin.site.register(DCRecord, DCRecordAdmin)
 admin.site.register(Header, HeaderAdmin)
 admin.site.register(MetadataFormat, MetadataFormatAdmin)
 admin.site.register(ResumptionToken, ResumptionTokenAdmin)
