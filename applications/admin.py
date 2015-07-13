c6c4eaa5ff36bb3d1aaa29039caa5e5d529aab95
jnphilipp <me@jnphilipp.org>
2015-07-13 17:05:40 +0200

    Updated applications admin.

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,18 @@
+from applications.models import Application, TextFieldSingleLine
 from django.contrib import admin
+from django.forms import TextInput
 
-# Register your models here.
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'client_id', 'secret', 'updated_at')
+    readonly_fields = ('client_id', 'secret')
+    search_fields = ('name',)
+
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off'})},
+    }
+
+    fieldsets = [
+        (None, {'fields': ['name', 'client_id', 'secret']}),
+    ]
+
+admin.site.register(Application, ApplicationAdmin)
--- applications/models.py
+++ applications/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from os import urandom
 
 class TextFieldSingleLine(models.TextField):
     pass
